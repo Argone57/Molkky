91b8355a381cfa700cd9aebd95a7242628860a7c
Argone <altirax@hotmail.com>
2026-07-29 02:44:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='molkky-v46';
+const CACHE='molkky-v47';
 const FILES=['./','index.html','styles.css','setup-fixes.css','app.js','manifest.webmanifest','placement-quilles.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
